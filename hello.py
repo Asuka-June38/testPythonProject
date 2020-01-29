3d5e4318a26421b3137857c4d0bb73a9a7604edb
Asuka <asukasmit@gmail.com>
2020-01-29 19:40:52 +0700

    add pathes for methods, create method for db.

--- hello.py
+++ hello.py
@@ -1,15 +1,24 @@
 from flask import Flask, send_file
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello 1World!"
+    return "Hello World!"
 
-def give_json():
+
+@app.route("/json/")
+def show_json():
     return send_file('convertcsv.json')
 
 
+@app.route("/db/")
+def show_db():
+    pass
+
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
